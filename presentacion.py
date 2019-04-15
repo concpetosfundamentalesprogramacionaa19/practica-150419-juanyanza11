73c98dbd548b78d4f6f243135054515adc1f9483
juanyanza11 <ww4fjj@gmail.com>
2019-04-15 16:46:06 -0500

    Subiendo

--- presentacion.py
+++ presentacion.py
@@ -5,5 +5,5 @@ presentación Nombres y Apellidos
 """
 nombre="Juan"
 apellido="Yanza"
-bs= ' '
-print("Hola me llamo "+nombre+bs+apellido)	
+
+print("Hola me llamo "+nombre,apellido)	
---
+++ presentacion2.py
@@ -0,0 +1,18 @@
+"""
+INGRESO DE INFORMACIÓN POR TECLADO
+USUARIO: juanyanza11
+"""
+
+nombre= input("Por favor ingrese su nombre:\n")
+apellido= input("Por favor ingrese su apellido\n")
+#print("%s""-""-%s" %(nombre, apellido))
+#print(nombre,apellido)
+
+"""
+Mi nombre es: Juan 
+		mi apellido es:
+Yanza
+"""
+
+print("Mi nombre es: %s\n \t Mi apellido es:\n%s"%(nombre,apellido))
+
