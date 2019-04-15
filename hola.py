f9738e7da4747145984b05f6d0c90a2d8fee49ac
juanyanza11 <ww4fjj@gmail.com>
2019-04-15 16:24:01 -0500

    Subiendo ejemplos

---
+++ hola.py
@@ -0,0 +1,6 @@
+"""Este es mi primer ejemplo en lenguaje python
+	de varias líneas
+"""
+	
+print("Hola Mundo UTPL")
+
---
+++ presentacion.py
@@ -0,0 +1,9 @@
+"""" 
+Comentario:
+presentación Nombres y Apellidos
+
+"""
+nombre="Juan"
+apellido="Yanza"
+bs= ' '
+print("Hola me llamo "+nombre+bs+apellido)	
